05a828839be3bc30fd072029f7c82774d259ce0f
landy <landy0602@gmail.com>
2024-03-13 14:05:48 +0800

    更改js

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const path = require("path");
-const { fileURLToPath } = require('url');
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+
 app.use(express.static(path.resolve(__dirname, "public")));
-app.use("/bootstrap", express.static(path.resolve(__dirname, "node_modules/bootstrap/dist")));
-app.use("/jquery", express.static(path.resolve(__dirname, "node_modules/jquery/dist")))
-app.use('/fontawesome', express.static(path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free')));
+app.use(
+  "/bootstrap",
+  express.static(path.resolve(__dirname, "node_modules/bootstrap/dist"))
+);
+app.use(
+  "/fontawesome",
+  express.static(
+    path.resolve(__dirname, "node_modules/@fortawesome/fontawesome-free")
+  )
+);
+app.use(
+  "/jquery",
+  express.static(path.resolve(__dirname, "node_modules/jquery/dist"))
+);
 
 app.get("/", (req, res) => {
-  res.send("網站首頁")
+  res.send("網站首頁");
 });
 
-
 app.listen(3000, () => {
-  console.log("伺服器已啟動於 http://localhost:3000");
+  console.log("伺服器已啟動 http://localhost:3000");
 });
