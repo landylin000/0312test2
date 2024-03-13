50a694c85f0c6f4a82f448259b17a113e0ed2474
landy <landy0602@gmail.com>
2024-03-13 14:01:40 +0800

    更改js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require("express");
-const { resolve } = require("path");
-const { dirname } = require("path");
-const { fileURLToPath } = require("url");
+const path = require("path");
+const { fileURLToPath } = require('url');
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-app.use(express.static(resolve(__dirname, "public")));
-app.use("/bootstrap", express.static(resolve(__dirname, "node_modules/bootstrap/dist")));
-app.use("/jquery", express.static(resolve(__dirname, "node_modules/jquery/dist")))
-app.use('/fontawesome', express.static(resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free')));
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use("/bootstrap", express.static(path.resolve(__dirname, "node_modules/bootstrap/dist")));
+app.use("/jquery", express.static(path.resolve(__dirname, "node_modules/jquery/dist")))
+app.use('/fontawesome', express.static(path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free')));
 
 app.get("/", (req, res) => {
-  res.send("網站首頁");
+  res.send("網站首頁")
 });
+
 
 app.listen(3000, () => {
   console.log("伺服器已啟動於 http://localhost:3000");
